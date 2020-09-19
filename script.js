718b9e51dbb405876c310f71213be2b0785126be
Solosgit <71104314+Solosgit@users.noreply.github.com>
2020-09-19 12:33:12 -0400

    my first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var boxEle = document.body.querySelector(".box");
+
+var inputOne = prompt("input a number");
+var inputTwo = prompt("input a number");
+
+boxEle.innerHTML= parseInt(inputOne) + parseInt(inputTwo);
